7c6b7471456bedd4be96deb2a2e062d69268b5b1
Azra-Rizqia <azrarizqia@gmail.com>
2023-10-31 09:14:22 +0700

    add part 3

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
 const express = require('express')
 const app = express()
+const userrouter = require('./router/users')
+
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/users', (req, res) => {
-  res.send('Get All user')
-})
-
-app.post('/users', (req, res) => {
-  res.send('Got a Post user')
-})
-
-app.put('/users/:id', (req, res) => {
-  res.send('Got a update user')
-})
-
-app.delete('/users/:id', (req, res) => {
-  res.send('Got a delete user')
-})
+app.use(userrouter)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
